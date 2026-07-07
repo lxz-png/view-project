1a11f1d6f913c948df7d3d95f484b100dfb6eecf
lxz-png <3403331832@qq.com>
2026-07-07 15:45:12 +0800

    Delete check_gpu.py

--- check_gpu.py
+++
@@ -1,7 +0,0 @@
-[check_gpu.py](https://github.com/user-attachments/files/29734407/check_gpu.py)
-# view-project
-view-project
-import torch
-print("CUDA是否可用：", torch.cuda.is_available())
-print("显卡名称：", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "无GPU")
-print(torch.version.cuda)
